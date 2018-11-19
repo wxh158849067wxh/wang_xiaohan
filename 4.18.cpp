84e63531867967e5ce6155d28a53bfd71dbec552
DESKTOP-F2LO12M\王小涵 <158849067@qq.com>
2018-11-19 22:50:08 +0800

    第四章18题

---
+++ 4.18.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n = 0;
+
+
+   cout << "N\t10*N\t100*N\t1000*N\n\n";
+
+   while ( ++n <= 5 )
+   {
+
+      cout << n << '\t' << 10 * n << '\t' << 100 * n
+         << '\t' << 1000 * n << '\n';
+   }
+   cout << endl;
+}
+
+

2b88d0638148a08f09d2f4304e8b1968fe35491b
DESKTOP-F2LO12M\王小涵 <158849067@qq.com>
2018-11-20 17:28:01 +0800

    第四章30题

---
+++ 4.30/main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   double radius;
+   double pi = 3.14159;
+
+
+   cout << "Enter the radius: ";
+   cin >> radius;
+
+   cout << "The diameter is " << radius * 2.0;
+
+
+   cout << "\nThe circumference is " << 2.0 * pi * radius;
+
+
+   cout << "\nThe area is " << pi * radius * radius << endl;
+}
+

0e9884f22e6f3af0ca3c9bc52f98bd6327c8a0b0
DESKTOP-F2LO12M\王小涵 <158849067@qq.com>
2018-12-12 18:13:23 +0800

    第五章15题

---
+++ 5.15/main.cpp
@@ -0,0 +1,15 @@
+
+
+#include "GradeBook.h"
+
+int main()
+{
+
+   GradeBook myGradeBook( "CS101 C++ Programming" );
+
+   myGradeBook.displayMessage();
+   myGradeBook.inputGrades();
+   myGradeBook.displayGradeReport();
+}
+
+
